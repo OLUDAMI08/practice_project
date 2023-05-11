fba341959a65cfb6348b211628f0e729d760171d
Lawal Damilare Toheeb <lawaltd088@gmail.com>
2023-05-11 17:02:47 +0100

    dital clock

---
+++ index.js
@@ -0,0 +1,24 @@
+const hourEl = document.querySelector("#hour");
+const minuteEl = document.querySelector("#minute");
+const secondEl = document.querySelector("#second");
+const ampms = document.querySelector(".ampm");
+
+function digitalClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  if (h > 12) {
+    h = h - 12;
+    ampms.innerHTML = "PM";
+  }
+  if (h < 10) {
+    h = "0" + h;
+  }
+
+  hourEl.innerHTML = h;
+  minuteEl.innerHTML = m;
+  secondEl.innerHTML = s;
+  setTimeout(digitalClock, 1000);
+}
+digitalClock();
